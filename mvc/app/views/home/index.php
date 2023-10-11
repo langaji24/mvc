92b12afbd3638b02aac32d31023e418e30d53042
langaji24 <ajibaskaragilang@gmail.com>
2023-10-11 07:58:19 +0700

    mvc

---
+++ mvc/app/views/home/index.php
@@ -0,0 +1,15 @@
+<div class ="container">
+    <div class =" py-4 px-5">
+        <h1 class ="display-4">Selamat Datang!</h1>
+        <hr class ="my-4">
+        <p class="fw-bold fs-5">Inventaris Alat Peminjaman</p>
+        <br>
+        <br>
+        <p class="fw-bolder">Peraturan Peminjaman</p>
+    <ol>
+        <li>Menemui Petugas Inventaris ketika meminjam atau mengembalikan barang</li>
+        <li>Lama waktu peminjaman hanya 2 hari</li>
+        <li>Menyerahkan kartu identitas ketika malakukan peminjaman sebagai jaminan</li>
+    </ol>
+    </div>
+</div>
